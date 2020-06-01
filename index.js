d2d8669ea8af35e47f7717a682cbebb1c48e7e27
Lo Van Long <longlv@v-auto.com.vn>
2020-06-01 22:25:17 +0700

    setup

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
-require("dotenv").config();
-const express = require('express')
-const app = express()
-const port = 3000
-
-const { sequelize } = require('./models')
-
-// sequelize.sync({ force: true })
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-        sequelize.sync({ force: true })
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const models = require('./models')
+module.exports = models
