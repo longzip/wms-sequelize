f9a86c89121fb2b32bf964e73b6581fb13e508aa
byndiorpro182 <34688488+byndiorpro182@users.noreply.github.com>
2020-08-11 16:52:55 +0700

    fix

---
+++ migrations/20200811094632-create-request.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Requests', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+      },
+      costcenterId: {
+        type: Sequelize.UUID
+      },
+      companyId: {
+        type: Sequelize.UUID
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Requests');
+  }
+};
